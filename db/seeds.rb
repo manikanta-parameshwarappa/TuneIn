a5d0eac27faaf203866afc9fd5b99313da8af71f
Manikanta Parameswarappa <manikantap036@gmail.com>
2026-03-16 00:33:38 +0530

    add-admin-seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,11 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+User.create!(
+  name: "Admin User",
+  email: "admin@example.com",
+  password: "securepassword123",
+  password_confirmation: "securepassword123",
+  dob: Date.new(1990, 1, 1),
+  role: "admin"
+)
