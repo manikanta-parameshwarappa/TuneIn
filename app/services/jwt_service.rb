1da00c4cb0ac33557ecc80d1e93d2a92e957f219
Manikanta Parameswarappa <manikantap036@gmail.com>
2026-03-19 16:12:59 +0530

    add-jwt-encode-decode-algo

--- app/services/jwt_service.rb
+++ app/services/jwt_service.rb
@@ -3,15 +3,13 @@ class JwtService
 
   def self.encode(payload, exp = 15.minutes.from_now)
     payload[:exp] = exp.to_i
-    JWT.encode(payload, SECRET)
+    JWT.encode(payload, SECRET, 'HS256') # explicitly set algorithm
   end
 
   def self.decode(token)
-    decoded = JWT.decode(token, SECRET)[0]
+    decoded = JWT.decode(token, SECRET, true, { algorithm: 'HS256' })[0]
     HashWithIndifferentAccess.new(decoded)
-  rescue JWT::ExpiredSignature
-    nil
-  rescue JWT::DecodeError
+  rescue JWT::ExpiredSignature, JWT::DecodeError
     nil
   end
 end
