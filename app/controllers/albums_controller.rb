90266fa70416a54451e628a33098537dda1ef380
Manikanta Parameswarappa <manikantap036@gmail.com>
2026-05-15 12:41:50 +0530

    fix-album-crud-issues

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -3,23 +3,24 @@ class AlbumsController < ApplicationController
 
   before_action :authorize_admin!, only: [:create, :update, :destroy]
 
-   # GET /albums
+  # GET /albums
   def index
-    albums = Album.all
-    render json: albums
+    albums = Album.includes(artist: :user).all
+    render json: albums.map { |album| serialize_album(album) }
   end
 
   # GET /albums/:id
   def show
-    album = Album.find(params[:id])
-    render json: album, include: :songs
+    album = Album.includes(:songs, artist: :user).find(params[:id])
+    render json: serialize_album(album).merge(songs: album.songs)
   end
 
   # POST /albums
   def create
     album = Album.new(album_params)
     if album.save
-      render json: album, status: :created
+      album = Album.includes(artist: :user).find(album.id)
+      render json: serialize_album(album), status: :created
     else
       render json: album.errors, status: :unprocessable_entity
     end
@@ -27,9 +28,9 @@ class AlbumsController < ApplicationController
 
   # PUT/PATCH /albums/:id
   def update
-    album = Album.find(params[:id])
+    album = Album.includes(artist: :user).find(params[:id])
     if album.update(album_params)
-      render json: album
+      render json: serialize_album(album)
     else
       render json: album.errors, status: :unprocessable_entity
     end
@@ -43,6 +44,28 @@ class AlbumsController < ApplicationController
   end
 
   private
+
+  def serialize_album(album)
+    cover_url = album.cover_image.attached? \
+      ? rails_blob_path(album.cover_image, only_path: true) \
+      : nil
+
+    {
+      id: album.id,
+      name: album.name,
+      released_date: album.released_date,
+      description: album.description,
+      artist_id: album.artist_id,
+      cover_image_url: cover_url,
+      artist: album.artist ? {
+        id: album.artist.id,
+        name: album.artist.user&.name,
+        email: album.artist.user&.email
+      } : nil,
+      created_at: album.created_at,
+      updated_at: album.updated_at
+    }
+  end
 
   def authorize_admin!
     unless current_user&.role == "admin"
